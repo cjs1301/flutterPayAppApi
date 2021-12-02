940c4ff0349cf486e5acbe5b2cd4a09d75a7e612
최재송 <jaesong@primeblock.co>
2021-12-02 12:13:11 +0900

    푸쉬알림 수정

--- controllers/admin/rtpay.js
+++ controllers/admin/rtpay.js
@@ -202,13 +202,13 @@ module.exports = {
                     message: "유효하지 않은 토큰 입니다.",
                 });
             }
-            const { word, date, limit, pageNum } = req.query;
+            const { word, limit, pageNum } = req.query;
             let offset = 0;
 
             if (pageNum > 1) {
                 offset = limit * (pageNum - 1);
             }
-            if (word === undefined || date === undefined) {
+            if (word === undefined) {
                 return res.status(400).send({
                     data: null,
                     message: "쿼리항목이 빠져 있습니다",
@@ -217,82 +217,12 @@ module.exports = {
 
             let result;
 
-            if (date) {
-                let [start, end] = date.split("~");
-                start = !start ? "1970-01-01" : start;
-                let startDay = new Date(start);
-                startDay.setHours(startDay.getHours() - 9);
-                let endDay = new Date(end);
-                endDay.setHours(endDay.getHours() + 15);
-                if (!word) {
-                    result = await rtpayLog.findAndCountAll({
-                        where: {
-                            createdAt: {
-                                [Op.between]: [startDay, endDay],
-                            },
-                        },
-                        limit: Number(limit),
-                        offset: Number(offset),
-                        order: [["createdAt", "DESC"]],
-                    });
-                    if (result) {
-                        result.total = (await rtpayLog.count())
-                            ? await rtpayLog.count()
-                            : 0;
-                        return res
-                            .status(200)
-                            .send({ data: result, message: "검색 완료" });
-                    }
-                }
-                if (word) {
-                    result = await rtpayLog.findAndCountAll({
-                        where: {
-                            [Op.or]: [
-                                {
-                                    title: {
-                                        [Op.like]: "%" + word + "%",
-                                    },
-                                },
-                                {
-                                    content: {
-                                        [Op.like]: "%" + word + "%",
-                                    },
-                                },
-                            ],
-                            createdAt: {
-                                [Op.between]: [startDay, endDay],
-                            },
-                        },
-                        order: [["createdAt", "DESC"]],
-                        limit: Number(limit),
-                        offset: Number(offset),
-                    });
-                    if (result) {
-                        result.total = (await rtpayLog.count())
-                            ? await rtpayLog.count()
-                            : 0;
-                        return res
-                            .status(200)
-                            .send({ data: result, message: "검색 완료" });
-                    }
-                }
-            }
-
-            if (!date && word) {
+            if (word) {
                 result = await rtpayLog.findAndCountAll({
                     where: {
-                        [Op.or]: [
-                            {
-                                title: {
-                                    [Op.like]: "%" + word + "%",
-                                },
-                            },
-                            {
-                                content: {
-                                    [Op.like]: "%" + word + "%",
-                                },
-                            },
-                        ],
+                        RNAME: {
+                            [Op.like]: "%" + word + "%",
+                        },
                     },
                     limit: Number(limit),
                     offset: Number(offset),
@@ -305,19 +235,15 @@ module.exports = {
                     .status(200)
                     .send({ data: result, message: "검색 완료" });
             }
-            if (!word && !date) {
-                result = await rtpayLog.findAndCountAll({
-                    limit: Number(limit),
-                    offset: Number(offset),
-                    order: [["createdAt", "DESC"]],
-                });
-                result.total = (await rtpayLog.count())
-                    ? await rtpayLog.count()
-                    : 0;
-                return res
-                    .status(200)
-                    .send({ data: result, message: "검색 완료" });
-            }
+            result = await rtpayLog.findAndCountAll({
+                limit: Number(limit),
+                offset: Number(offset),
+                order: [["createdAt", "DESC"]],
+            });
+            result.total = (await rtpayLog.count())
+                ? await rtpayLog.count()
+                : 0;
+            return res.status(200).send({ data: result, message: "검색 완료" });
         } catch (error) {
             console.log(error);
             return res.status(500).send({ data: result, message: "오류" });
